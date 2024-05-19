13b2a22e7ba5db60ac002b53e0359e5480d77ff7
nitipattime <nitipattime@gmail.com>
2024-05-19 20:48:37 +0700

    first commit

---
+++ aa.py
@@ -0,0 +1,13 @@
+import webbrowser as wb
+import time
+import pyautogui as pg
+
+time.sleep(2)
+wb.open('https://youtu.be/dQw4w9WgXcQ?si=fS_RVEXeuWfcA_lK')
+#wb.open('https://www.google.com')
+
+
+#pg.write('golang')
+#time.sleep(1)
+#pg.press('enter')
+#time.sleep(2)
